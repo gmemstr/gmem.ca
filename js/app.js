0493e6fe40b2d52773f106637dab99bd47699a54
Gabriel Simmer <gabriel@gitgalaxy.com>
2020-06-03 10:57:52 +0100

    Rewrite with async/await, tweak Experiment class.

--- js/app.js
+++ js/app.js
@@ -1,24 +1,22 @@
-const fetchSteamStatus = () => {
-    fetch("/api/steam")
-      .then((response) => {return response.json()})
-      .then((data) => {
-          let experiment = new Experiment("steam")
-          experiment.html`SteamID: ${data.SteamID} Online State: ${data.OnlineState}`
-       })
+const fetchSteamStatus = async () => {
+    let response = await fetch("https://cors-anywhere.herokuapp.com/https://gmem.ca/api/steam")
+    let data = await response.json()
+    let experiment = new Experiment("steam")
+    experiment.html`SteamID: ${data.SteamID} Online State: ${data.OnlineState}`
 }
 
-const loadApp = () => {
-    fetchSteamStatus()
+const loadApp = async () => {
+    await fetchSteamStatus()
 }
 window.onload = loadApp
 
 class Experiment {
     constructor(id) {
         this.id = id
+        this.element = document.getElementById(this.id)
     }
 
     html(strings, ...things) {
-        const element = document.getElementById(this.id)
         let x = document.createRange().createContextualFragment(
           strings.reduce(
             (markup, string, index) => {
@@ -33,7 +31,8 @@ class Experiment {
             ''
           )
         )
-        element.innerHTML = ""
-        element.append(x)
+        this.content = x
+        this.element.innerHTML = ""
+        this.element.append(x)
     }
 }
